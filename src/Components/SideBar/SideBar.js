535a5323c270fefd6eaae6a0ee25b81ec7b37e2b
Ashly Margaret <ashlymargaret030@gmail.com>
2022-12-28 18:29:16 +0530

    sidebar css updated

--- src/Components/SideBar/SideBar.js
+++ src/Components/SideBar/SideBar.js
@@ -1,37 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SideBar.css'
-// import Tree, { useTreeState } from 'react-hyper-tree'
 import { useNavigate } from 'react-router-dom'
 
 
-// const data = {
-//     id: 1,
-//     name: 'Job Listing',
-//     children: [
-//       {
-//         id: 2,
-//         name: 'Add New Job',
-        
-//       },
-//       {
-//         id: 2,
-//         name: 'Job Management',
-        
-        
-//       },
-//     ],
-//   }
-
-
-
 function SideBar() {
-    // const { required, handlers } = useTreeState({
-    //     data,
-    //     id: 'your_tree_id',
-    //     data: data,
-    //   defaultOpened: true,
-    //   multipleSelect: false,
-    //   })
+
+ const [show, setShow] = useState()
+   
 
     const navigate = useNavigate()
     
@@ -42,39 +17,43 @@ function SideBar() {
         </div>
         <div className="menuSection_box">
           
-        {/* <Tree
-      {...required}
-      {...handlers}
-      horizontalLineStyles={{
-        stroke: '#F0F0F0',
-        strokeWidth: 1,
-        strokeDasharray: '1 4',
-  }}
-  verticalLineStyles={{
-    stroke: '#F0F0F0',
-    strokeWidth: 1,
-    strokeDasharray: '1 4',
-}}
-/> */}
+   <ul className='menuUlConatiner'>
+    <li onClick={()=>{
+      navigate("*")
+    }}><i class="fa-solid fa-id-card"></i>Dashboard</li>
 
-   <ul>
-   <li onClick={()=>{
-      navigate("/")
-    }}>Add New Job <i className='angle' class="fa fa-angle-right" aria-hidden="true"></i>
-    </li>
-    <li onClick={()=>{
-      navigate("/joblist")
-    }}>Job Listing <i class="fa fa-angle-right " aria-hidden="true"></i>
-    </li>
-    <li onClick={()=>{
+    <li className='dropdown'  onClick={()=>{
+      setShow(!show)
+ 
+    }}> <a href="javascript:void(0)"><i class="fa-solid fa-pen-to-square"></i>Job Listing</a> </li>
+    {/* <a href="javascript:void(0)" className='dropbtn'>Job Listing</a> */}
+    
+       {
+        show ?  <div className="dropdown-content">
+       
+        <li onClick={()=>{
+          navigate("/")
+          }}> <a href="javascript:void(0)"><i className='angle' class="fa fa-angle-right" aria-hidden="true"></i>Add New Job  </a>
+        </li>
+        <li onClick={()=>{
+         navigate("/joblist")
+         }}> <a href="javascript:void(0)"> <i className='angle' class="fa fa-angle-right " aria-hidden="true"></i> Job Management 
+          </a> 
+        </li>
+     </div> : null
+       }
+    
+    
+  <li onClick={()=>{
       navigate("*")
-    }}>Upcoming Interviews  <i class="fa fa-angle-right" aria-hidden="true"></i>
+    }}> <i class="fa-solid fa-user"></i> Upcoming Interviews  
 </li>
-    <li onClick={()=>{
+
+  <li onClick={()=>{
       navigate("*")
-    }}>Users <i class="fa fa-angle-right" aria-hidden="true"></i>
-</li>
-   </ul> 
+    }}> <i class="fa-sharp fa-solid fa-id-card-clip"></i> Users</li>
+
+  </ul> 
         </div>
         <div className="settingAndLogoutSection">
             <button><i class="fa-sharp fa-solid fa-gear"></i> Settings</button>
